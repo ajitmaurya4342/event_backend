70616b60b8a9a3608732881ae4adb5f60297968b
Ajit <ajit.maurya@brij.tech>
2023-09-03 11:32:25 +0530

    Seat Type, :enre  and Language Master

--- src/modules/Master/MasterController.js
+++ src/modules/Master/MasterController.js
@@ -133,7 +133,7 @@ export async function addEditCities(req, res) {
     let obj = {
       city_name: city_name || null,
       city_is_active: city_is_active || 'Y',
-      country_id: country_id || 'Y',
+      country_id: country_id || null,
       ...dataReturnUpdate(user_info, isUpdate),
     };
     if (isUpdate) {
@@ -181,8 +181,239 @@ export async function getCityList(req, res) {
     .paginate(pagination(limit, currentPage));
 
   return res.send({
+    message: 'City List',
+    status: true,
+    Records: CountryList,
+  });
+}
+
+export async function addEditLanguages(req, res) {
+  let reqbody = req.body;
+  const { user_info } = req;
+  const { lang_name, lang_is_active, lang_iso2, lang_iso3, lang_id } = reqbody;
+  const isUpdate = lang_id ? true : false;
+  let checkFields = ['lang_name', 'lang_is_active'];
+  let result = await checkValidation(checkFields, reqbody);
+  if (!result.status) {
+    return res.send(result);
+  }
+
+  let checkLanguageExist = await global
+    .knexConnection('ms_languages')
+    .select(['lang_name', 'lang_is_active'])
+    .where(builder => {
+      builder.where({ lang_name });
+    })
+    .andWhere(builder => {
+      if (isUpdate) {
+        builder.whereNotIn('lang_id', [lang_id]);
+      }
+    });
+
+  if (checkLanguageExist.length) {
+    return res.status(200).json({
+      message: 'Language Already Exist',
+      status: false,
+      Records: checkLanguageExist,
+    });
+  } else {
+    let obj = {
+      lang_name: lang_name || null,
+      lang_iso2: lang_iso2 || null,
+      lang_iso3: lang_iso3 || null,
+      lang_is_active: lang_is_active || 'Y',
+      ...dataReturnUpdate(user_info, isUpdate),
+    };
+    if (isUpdate) {
+      await global.knexConnection('ms_languages').update(obj).where({ lang_id });
+    } else {
+      await global.knexConnection('ms_languages').insert(obj);
+    }
+
+    return res.send({
+      status: true,
+      message: `Language ${isUpdate ? 'Updated' : 'Created'} Successfully`,
+      obj,
+    });
+  }
+}
+export async function getLanguageList(req, res) {
+  const reqbody = { ...req.query, ...req.body };
+  const lang_id = reqbody.lang_id || null;
+  const limit = req.query.limit ? req.query.limit : 100;
+  const currentPage = req.query.currentPage ? req.query.currentPage : 1;
+
+  const CountryList = await global
+    .knexConnection('ms_languages')
+    .select(['lang_iso2', 'lang_iso3', 'lang_name', 'lang_id'])
+    .where(builder => {
+      if (lang_id) {
+        builder.where('lang_id', '=', lang_id);
+      }
+      if (req.query.search) {
+        builder.whereRaw(
+          ` concat_ws(' ',lang_name,lang_iso2,lang_iso3) like '%${req.query.search}%'`,
+        );
+      }
+    })
+    .orderBy('lang_id', 'desc')
+    .paginate(pagination(limit, currentPage));
+
+  return res.send({
     message: 'Country List',
     status: true,
     Records: CountryList,
+  });
+}
+
+export async function addEditGenre(req, res) {
+  let reqbody = req.body;
+  const { user_info } = req;
+  const { genre_name, genre_is_active, genre_id } = reqbody;
+  const isUpdate = genre_id ? true : false;
+  let checkFields = ['genre_name', 'genre_is_active'];
+  let result = await checkValidation(checkFields, reqbody);
+  if (!result.status) {
+    return res.send(result);
+  }
+
+  let checkGenreExist = await global
+    .knexConnection('ms_genre')
+    .select(['genre_name', 'genre_is_active'])
+    .where(builder => {
+      builder.where({ genre_name });
+    })
+    .andWhere(builder => {
+      if (isUpdate) {
+        builder.whereNotIn('genre_id', [genre_id]);
+      }
+    });
+
+  if (checkGenreExist.length) {
+    return res.status(200).json({
+      message: 'Genre Already Exist',
+      status: false,
+      Records: checkGenreExist,
+    });
+  } else {
+    let obj = {
+      genre_name: genre_name || null,
+      genre_is_active: genre_is_active || 'Y',
+      ...dataReturnUpdate(user_info, isUpdate),
+    };
+    if (isUpdate) {
+      await global.knexConnection('ms_genre').update(obj).where({ genre_id });
+    } else {
+      await global.knexConnection('ms_genre').insert(obj);
+    }
+
+    return res.send({
+      status: true,
+      message: `Genre ${isUpdate ? 'Updated' : 'Created'} Successfully`,
+      obj,
+    });
+  }
+}
+export async function getGenreList(req, res) {
+  const reqbody = { ...req.query, ...req.body };
+  const genre_id = reqbody.genre_id || null;
+  const limit = req.query.limit ? req.query.limit : 100;
+  const currentPage = req.query.currentPage ? req.query.currentPage : 1;
+
+  const GenreList = await global
+    .knexConnection('ms_genre')
+    .select(['genre_name', 'genre_id'])
+    .where(builder => {
+      if (genre_id) {
+        builder.where('genre_id', '=', genre_id);
+      }
+      if (req.query.search) {
+        builder.whereRaw(` concat_ws(' ',genre_name) like '%${req.query.search}%'`);
+      }
+    })
+    .orderBy('genre_id', 'desc')
+    .paginate(pagination(limit, currentPage));
+
+  return res.send({
+    message: 'Genre List',
+    status: true,
+    Records: GenreList,
+  });
+}
+
+export async function addEditSeatType(req, res) {
+  let reqbody = req.body;
+  const { user_info } = req;
+  const { seat_class_name, sct_is_active, sct_id } = reqbody;
+  const isUpdate = sct_id ? true : false;
+  let checkFields = ['seat_class_name', 'sct_is_active'];
+  let result = await checkValidation(checkFields, reqbody);
+  if (!result.status) {
+    return res.send(result);
+  }
+
+  let checkSeatTypeExist = await global
+    .knexConnection('ms_seat_class_type')
+    .select(['seat_class_name', 'sct_is_active'])
+    .where(builder => {
+      builder.where({ seat_class_name });
+    })
+    .andWhere(builder => {
+      if (isUpdate) {
+        builder.whereNotIn('sct_id', [sct_id]);
+      }
+    });
+
+  if (checkSeatTypeExist.length) {
+    return res.status(200).json({
+      message: 'Seat Type Already Exist',
+      status: false,
+      Records: checkSeatTypeExist,
+    });
+  } else {
+    let obj = {
+      seat_class_name: seat_class_name || null,
+      sct_is_active: sct_is_active || 'Y',
+      ...dataReturnUpdate(user_info, isUpdate),
+    };
+    if (isUpdate) {
+      await global.knexConnection('ms_seat_class_type').update(obj).where({ sct_id });
+    } else {
+      await global.knexConnection('ms_seat_class_type').insert(obj);
+    }
+
+    return res.send({
+      status: true,
+      message: `Seat Type ${isUpdate ? 'Updated' : 'Created'} Successfully`,
+      obj,
+    });
+  }
+}
+export async function getSeatTypeList(req, res) {
+  const reqbody = { ...req.query, ...req.body };
+  const sct_id = reqbody.sct_id || null;
+  const limit = req.query.limit ? req.query.limit : 100;
+  const currentPage = req.query.currentPage ? req.query.currentPage : 1;
+
+  const GenreList = await global
+    .knexConnection('ms_seat_class_type')
+    .select(['seat_class_name', 'sct_id'])
+    .where(builder => {
+      if (sct_id) {
+        builder.where('sct_id', '=', sct_id);
+      }
+      if (req.query.search) {
+        builder.whereRaw(
+          ` concat_ws(' ',seat_class_name) like '%${req.query.search}%'`,
+        );
+      }
+    })
+    .orderBy('sct_id', 'desc')
+    .paginate(pagination(limit, currentPage));
+
+  return res.send({
+    message: 'Seat Type List',
+    status: true,
+    Records: GenreList,
   });
 }
--- src/modules/Master/MasterRoutes.js
+++ src/modules/Master/MasterRoutes.js
@@ -4,8 +4,14 @@ import { checkSessionExist } from '@/middlewares/checkSessionExist';
 import {
   addEditCities,
   addEditCountries,
+  addEditGenre,
+  addEditLanguages,
+  addEditSeatType,
   getCityList,
   getCountryList,
+  getGenreList,
+  getLanguageList,
+  getSeatTypeList,
 } from '@/modules/Master/MasterController';
 
 const router = Router();
@@ -17,5 +23,13 @@ export function MasterRoutes() {
   router.post('/add-edit-cities', checkSessionExist, addEditCities);
   router.get('/getcitylist', checkSessionExist, getCityList);
 
+  router.post('/add-edit-languages', checkSessionExist, addEditLanguages);
+  router.get('/getlanguageslist', checkSessionExist, getLanguageList);
+
+  router.post('/add-edit-genres', checkSessionExist, addEditGenre);
+  router.get('/getgenreslist', checkSessionExist, getGenreList);
+
+  router.post('/add-edit-seattype', checkSessionExist, addEditSeatType);
+  router.get('/getseattypelist', checkSessionExist, getSeatTypeList);
   return router;
 }
