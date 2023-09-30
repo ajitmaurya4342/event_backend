86fe8a44f00943ff8977d51e5089ceaa1a8cd7b7
jitendrarik <jitendra@binarynumbers.io>
2023-09-30 14:50:03 +0530

    cross-origin

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ app.use(
 
 app.use(cors());
 app.use(hpp());
-app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '2mb' }));
 
